dc1390b09c339345271c4fae9d66a36b67383efb
Rickhighgadgets <kibirigeedrick5@gmail.com>
2025-06-20 20:25:20 -0400

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>RICK HIGH GADGETS</h1>
+        <p>Your one-stop shop for phones, computers, and accessories.</p>
+        <p>📞 +256770163447 | +256702297581 | +256768535897</p>
+        <p>📲 Telegram: @Levismiles</p>
+        <p>📍 Temangalo, Wakiso District, Uganda</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
